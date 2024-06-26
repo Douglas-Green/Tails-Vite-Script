708a062fe4ab6babb1460471e366acdfc133c386
Douglas-Green <varmevisible@gmail.com>
2024-06-26 11:21:40 -0400

    Updated files and added dependencies

--- .setup.sh
+++ .setup.sh
@@ -2,71 +2,60 @@
 
 # Function to check if a command exists
 command_exists() {
-    command -v "$1" &> /dev/null
+    command -v "$1" >/dev/null 2>&1
 }
 
 # Function to create a directory with error handling
 create_directory() {
-    local dir=$1
-    if mkdir -p "$dir"; then
-        echo -e "\e[32mCreated directory: $dir\e[0m"
+    local dir_path="$1"
+    if mkdir -p "$dir_path"; then
+        echo -e "\e[32mCreated directory: $dir_path\e[0m"
     else
-        echo -e "\e[31mFailed to create directory: $dir\e[0m"
+        echo -e "\e[31mFailed to create directory: $dir_path\e[0m"
     fi
 }
 
 # Function to remove a directory or file with error handling
 remove_item_with_handling() {
-    local item=$1
-    if rm -rf "$item"; then
-        echo -e "\e[32mRemoved: $item\e[0m"
+    local path="$1"
+    if rm -rf "$path"; then
+        echo -e "\e[32mRemoved: $path\e[0m"
     else
-        echo -e "\e[31mFailed to remove: $item\e[0m"
+        echo -e "\e[31mFailed to remove: $path\e[0m"
     fi
 }
 
 # Function to execute a command with error handling
 execute_command() {
-    local cmd=$1
-    if eval "$cmd"; then
-        echo -e "\e[32mSuccessfully executed: $cmd\e[0m"
+    local cmd="$1"
+    local args="$2"
+    if $cmd $args; then
+        echo -e "\e[32mSuccessfully executed: $cmd $args\e[0m"
     else
-        echo -e "\e[31mError executing: $cmd\e[0m"
+        echo -e "\e[31mError executing: $cmd $args\e[0m"
     fi
 }
 
 # Create Server directory
-echo -e "\n\e[36mSetting up the Server directory...\e[0m"
+echo -e "\e[36m\nSetting up the Server directory...\e[0m"
 create_directory "Server"
-
-# Wait for 2 seconds
-sleep 2
 
 # Setup Server
 cd "Server" || exit
-echo -e "\n\e[36mInitializing new Node.js project in Server directory...\e[0m"
-execute_command "npm init -y"
-
-# Wait for 2 seconds
-sleep 2
+echo -e "\e[36m\nInitializing new Node.js project in Server directory...\e[0m"
+execute_command "npm" "init -y"
 
 # Install server dependencies
-echo -e "\n\e[36mInstalling server dependencies...\e[0m"
-execute_command "npm install express dotenv mysql2 sequelize sequelize-cli jsonwebtoken bcryptjs cors colorette"
-
-# Wait for 2 seconds
-sleep 2
+echo -e "\e[36m\nInstalling server dependencies...\e[0m"
+execute_command "npm" "install express dotenv mysql2 sequelize sequelize-cli jsonwebtoken bcryptjs cors colorette nodemon"
 
 # Create necessary directories and files
-echo -e "\n\e[36mCreating necessary directories and files in Server directory...\e[0m"
+echo -e "\e[36m\nCreating necessary directories and files in Server directory...\e[0m"
 for dir in src/config src/controllers src/middleware src/migrations src/models src/routes src/template; do
     create_directory "$dir"
 done
 
-# Wait for 2 seconds
-sleep 2
-
-for file in ".env" "App.js" "santasList.txt" "server.js" "src/config/config.js" "src/controllers/controller.js" "src/middleware/middleware.js" "src/migrations/migration.js" "src/models/model.js" "src/routes/route.js" "src/template/template.js"; do
+for file in .env App.js santasList.txt Server.js src/config/config.js src/controllers/index.js src/middleware/index.js src/migrations/index.js src/models/index.js src/routes/index.js src/template/index.js; do
     if touch "$file"; then
         echo -e "\e[32mCreated file: $file\e[0m"
     else
@@ -74,93 +63,53 @@ for file in ".env" "App.js" "santasList.txt" "server.js" "src/config/config.js" 
     fi
 done
 
-# Wait for 2 seconds
-sleep 2
-
-# Navigate back to the root directory
 cd ..
 
-# Wait for 2 seconds
-sleep 2
-
 # Create Client directory
-echo -e "\n\e[36mSetting up the Client directory...\e[0m"
+echo -e "\e[36m\nSetting up the Client directory...\e[0m"
 create_directory "Client"
-
-# Wait for 2 seconds
-sleep 2
 
 # Setup Client
 cd "Client" || exit
-APP_NAME=$(basename "$(pwd)")
-echo -e "\n\e[36mCreating a new Vite app named $APP_NAME with React and JavaScript template...\e[0m"
-execute_command "npm create vite@latest -- --template react"
-
-# Wait for the Vite app creation process to start
-sleep 3
-
-# Simulate pressing the period key and then Enter key twice
-xdotool type "."
-sleep 1
-xdotool key Return
-sleep 1
-xdotool key Return
+APP_NAME=${PWD##*/}
+echo -e "\e[36m\nCreating a new Vite app named $APP_NAME with React and JavaScript template...\e[0m"
+execute_command "npm" "create vite@latest -- --template react"
 
 # Install client dependencies
-echo -e "\n\e[36mInitializing client dependencies...\e[0m"
-sleep 2
-echo -e "\n\e[33mPhase 1 initiated.\e[0m"
-execute_command "npm install axios react-router-dom framer-motion @headlessui/react @emotion/react @emotion/styled"
-sleep 2
+echo -e "\e[36m\nInstalling client dependencies...\e[0m"
+execute_command "npm" "install axios react-router-dom framer-motion @headlessui/react @emotion/react @emotion/styled prop-types react-select"
 
 # Install Tailwind CSS and other necessary packages
-echo -e "\n\e[36mInitializing Tailwind CSS, Autoprefixer, and PostCSS...\e[0m"
-sleep 2
-echo -e "\n\e[33mPhase 2 initiated.\e[0m"
-execute_command "npm install -D tailwindcss@latest postcss@latest autoprefixer@latest"
-sleep 2
+echo -e "\e[36m\nInstalling Tailwind CSS, Autoprefixer, and PostCSS...\e[0m"
+execute_command "npm" "install -D tailwindcss@latest postcss@latest autoprefixer@latest"
 
 # Initialize Tailwind CSS
-echo -e "\n\e[36mInitializing Tailwind CSS for enhanced stylization and functionality...\e[0m"
-sleep 2
-echo -e "\n\e[33mPhase 3 initiated.\e[0m"
-execute_command "npx tailwindcss init -p"
-sleep 2
+echo -e "\e[36m\nInitializing Tailwind CSS...\e[0m"
+execute_command "npx" "tailwindcss init -p"
 
 # Configure Tailwind CSS in index.css
-echo -e "\n\e[36mAttempting to store Tailwind CSS directives in index.css...\n\e[0m"
-sleep 2
-echo -e "\n\e[33mFinalizing Phases 1-3... Please wait... Phase 4 initiated.\n\e[0m"
-cat <<EOT > ./src/index.css
-@tailwind base;
+echo -e "\e[36m\nConfiguring Tailwind CSS in index.css...\e[0m"
+echo "@tailwind base;
 @tailwind components;
-@tailwind utilities;
-EOT
-echo -e "\e[32mDirectives stored in index.css. That completes all Tailwind CSS configurations. Thank you for your patience. You may now move on to the important stuff.\e[0m"
-
-# Wait for 5 seconds
-sleep 5
-
-# Navigate into the src directory
-cd "src" || exit
+@tailwind utilities;" > ./src/index.css
+echo -e "\e[32mTailwind CSS directives added to index.css.\e[0m"
 
 # Create additional directories in the src directory
-echo -e "\n\e[36mCreating additional directories in the src directory...\e[0m"
+echo -e "\e[36m\nCreating additional directories in the src directory...\e[0m"
+cd src || exit
 for dir in authentication components pages services theme utils; do
     create_directory "$dir"
 done
 
 # Remove specified items
-echo -e "\n\e[36mRemoving specified items...\e[0m"
+echo -e "\e[36m\nRemoving specified items...\e[0m"
 remove_item_with_handling "../assets"
 remove_item_with_handling "App.css"
 
-# Wait for 2 seconds
-sleep 2
+cd ..
 
 # Final message recap
-final_message="
-Setup complete! The following tasks were performed:
+echo -e "\e[36m\nSetup complete! The following tasks were performed:
 1. Created and set up Server directory.
 2. Initialized Node.js project and installed server dependencies.
 3. Created necessary directories and files in Server.
@@ -171,14 +120,9 @@ Setup complete! The following tasks were performed:
 8. Created additional directories in src.
 9. Removed unnecessary items.
 
-Thank you for using the script!
-"
-echo -e "\n\e[36m$final_message\e[0m"
+Thank you for using the script!\e[0m"
 
-# Wait for 10 seconds before exiting
-sleep 10
-
-cat << "EOF"
+echo -e "\e[36m
 # In code we trust. All others bring data.
 #  _    _  ____  ___    .  .     _  ___    .  .   __   ___   ___
 # ( )  ( )(  __)( o )  _\`'/_   ( )/  _)  _\`'/_ (  ) (   ) /  _)
@@ -186,4 +130,7 @@ cat << "EOF"
 # ( '  ` )(  _) ( O  )  /'`\ ((_( ) __) )  /'`\ ( O  )( __/  __) )
 #  \_/\_/ /____\/___/         \__/ /___/         \__/ /_\   /___/
 # Doug_Dev 2024
-EOF
+\e[0m"
+
+# Wait for 10 seconds before exiting
+sleep 10
